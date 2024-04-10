1c68494ee3dc8220b6d99fa2dd63235d688fd0a3
m.glowacki <m.glowacki@smsnet.pl>
2024-04-10 15:47:49 +0200

    Initial

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Button from "./interface/Button";
+import SearchBar from "./interface/SearchBar";
+import CustomFilter from "./interface/CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+
+export {
+  Hero,
+  Navbar,
+  Footer,
+  Button,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
